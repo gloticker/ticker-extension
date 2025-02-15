6f611b502802b95abd3145e34e5a0b38885e937f
takealook97 <takealook97@naver.com>
2025-02-15 13:36:00 +0900

    chore: add favicon and extension icons with build configuration updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
-import { ManifestV3Export } from '@crxjs/vite-plugin'
+import { defineConfig } from "vite";
+import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest: manifest as ManifestV3Export }),
-  ],
-})
+  plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
+  resolve: {
+    alias: {
+      "@logo": path.resolve(process.cwd(), "@logo"),
+    },
+  },
+  build: {
+    assetsDir: "assets",
+    copyPublicDir: true,
+    outDir: "dist",
+  },
+});
