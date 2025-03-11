26b72c3b9670ec4c20787fe2eca97ddc1f629ce7
takealook97 <takealook97@naver.com>
2025-03-12 06:10:29 +0900

    delete: remove console log on background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,6 @@
 export {};
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-
   chrome.storage.local.set({
     initialized: true,
     lastUpdate: new Date().toISOString(),
